865142696ea56e348b36e147e7f38536c82ea584
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 19:12:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <title>Enrick Gallery</title>
   </head>
   <body>
-    <h3>Gornez, John Enrick Photo Gallery</h3>
-    <p>List of Photos</p>
+    <h3 align="center">Gornez, John Enrick Photo Gallery</h3>
+    <p align="center">List of Photos</p>
+    <a href="#" align="center">Image 1</a>
+    <a href="#" align="center">Image 2</a>
+    <a href="#" align="center">Image 3</a>
   </body>
 </html>
