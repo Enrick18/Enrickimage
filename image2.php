a0e04c8b16464c31471e59a73c7c8f7d0078d0fc
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 19:59:57 +0800

    Update image2.php

--- image2.php
+++ image2.php
@@ -3,6 +3,7 @@
   <head>
   </head>
   <body>
+    <h3 align="center">Gornez, John Enrick Photo Gallery</h3>
     <main align="center">
     <a href="index.php">Home Page</a>
     <a href="image3.php">Next</a>
