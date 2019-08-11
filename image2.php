0e9feaf1f3d05ca4ed4a4eb30b0b54dc4293d25a
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 22:22:30 +0800

    Update image2.php

--- image2.php
+++ image2.php
@@ -3,8 +3,8 @@
   <head>
   </head>
   <body bgcolor="Black">
-    <h3 align="center" color="white">Gornez, John Enrick Digi Art Gallery</h3>
-    <main align="center" color="white">
+    <h3 align="center"><font color="white">Gornez, John Enrick Digi Art Gallery</font></h3>
+    <main align="center">
       <img src="Image015.jpg" alt="Digi Art 2" width="1000" height="600"><br>
     <a href="index.php">Home Page</a>
     <a href="image3.php">Next</a>
