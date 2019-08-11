b42f6b1187243f48da16777b1f2324af570dcdc0
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 18:27:41 +0800

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Enrick Gallery</title>
+  </head>
+  <body>
+    <h3>Gornez, John Enrick Photo Gallery</h3>
+    <p>List of Photos</p>
+  </body>
+</html>
