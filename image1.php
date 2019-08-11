951d7e4feaafc230c9e29fe5437f3cda988af091
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 22:08:48 +0800

    Update image1.php

--- image1.php
+++ image1.php
@@ -5,7 +5,7 @@
   <body>
     <h3 align="center">Gornez, John Enrick Photo Gallery</h3>
     <main align="center">
-      <img src="Image024.jpg" alt="Digi art 1" width="500" height="600"><br>
+      <img src="Image024.jpg" alt="Digi art 1" width="500" height="800"><br>
     <a href="index.php">Home Page</a>
     <a href="image2.php">Next</a>
     </main>
