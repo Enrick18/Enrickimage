ac8eaa2884c2deec155036ff4a712f08114bec94
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 22:24:13 +0800

    Update image1.php

--- image1.php
+++ image1.php
@@ -3,8 +3,8 @@
   <head>
   </head>
   <body bgcolor="Black">
-    <h3 align="center" color="white">Gornez, John Enrick Digi Ar Gallery</h3>
-    <main align="center" color="white">
+    <h3 align="center"><font color="white">Gornez, John Enrick Digi Ar Gallery</font></h3>
+    <main align="center">
       <img src="Image024.jpg" alt="Digi art 1" width="500" height="800"><br>
     <a href="index.php">Home Page</a>
     <a href="image2.php">Next</a>
