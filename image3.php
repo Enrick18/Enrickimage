3b6cbafcbfcb2cf11842e058dd0f00ab23a2a4b0
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 22:06:44 +0800

    Update image3.php

--- image3.php
+++ image3.php
@@ -5,7 +5,7 @@
   <body>
     <h3 align="center">Gornez, John Enrick Photo Gallery</h3>
     <main align="center">
-    <img src="Imagee.jpg" alt="Digi art 3"><br>
+    <img src="Imagee.jpg" alt="Digi art 3" width="500" height="600"><br>
     <a href="index.php">Home Page</a>
     </main>
   </body>
