8f7a0475f2ddb579105b1a5146f719f5fb15637f
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 22:23:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <title>Enrick Gallery</title>
   </head>
   <body bgcolor="Black">
-    <h3 align="center" color="white">Gornez, John Enrick Digi Art Gallery</h3>
-    <p align="center" color="white">List of Photos</p>
-    <main align="center" color="white">
+    <h3 align="center"><font color="white">Gornez, John Enrick Digi Art Gallery</font></h3>
+    <p align="center"><font color="white">List of Photos</font></p>
+    <main align="center">
     <a href="image1.php">Image 1</a><br>
     <a href="image2.php">Image 2</a><br>
     <a href="image3.php">Image 3</a>
