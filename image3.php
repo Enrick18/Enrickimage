473b44b312909e1cf913d5fe83207a22b28b7577
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 22:23:03 +0800

    Update image3.php

--- image3.php
+++ image3.php
@@ -3,8 +3,8 @@
   <head>
   </head>
   <body bgcolor="Black">
-    <h3 align="center" color="white">Gornez, John Enrick Digi Art Gallery</h3>
-    <main align="center" color="white">
+    <h3 align="center"><font color="white">Gornez, John Enrick Digi Art Gallery</font></h3>
+    <main align="center">
     <img src="Imagee.jpg" alt="Digi art 3" width="900" height="500"><br>
     <a href="index.php">Home Page</a>
     </main>
