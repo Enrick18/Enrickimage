ef2c38218e4ddb988838dc0ef7d7b5c5c8588942
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 20:25:21 +0800

    Update image3.php

--- image3.php
+++ image3.php
@@ -5,7 +5,7 @@
   <body>
     <h3 align="center">Gornez, John Enrick Photo Gallery</h3>
     <main align="center">
-    <img scr="Commuter Account5.png" alt="Digi art 3"><br>
+    <img scr="Imagee.jpg" alt="Digi art 3"><br>
     <a href="index.php">Home Page</a>
     </main>
   </body>
