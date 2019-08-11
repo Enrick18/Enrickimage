45094cbcfdb389c5878e90cd3811dba26c5070f7
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 20:13:27 +0800

    Update image2.php

--- image2.php
+++ image2.php
@@ -4,8 +4,8 @@
   </head>
   <body>
     <h3 align="center">Gornez, John Enrick Photo Gallery</h3>
-    <img scr="Commuter Account7.png" alt="Digi Art 2">
     <main align="center">
+      <img scr="Commuter Account7.png" alt="Digi Art 2">
     <a href="index.php">Home Page</a>
     <a href="image3.php">Next</a>
     </main>
