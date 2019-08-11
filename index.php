c638669886f53495346123503e9aa39f11c324fb
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-11 19:27:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <p align="center">List of Photos</p>
     <main align="center">
     <a href="image1.php">Image 1</a><br>
-    <a href="#">Image 2</a><br>
-    <a href="#">Image 3</a>
+    <a href="image2.php">Image 2</a><br>
+    <a href="image3.php">Image 3</a>
     </main>
   </body>
 </html>
