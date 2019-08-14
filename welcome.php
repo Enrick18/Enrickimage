496ba894676469b27909f3df5da36b0b21f63bd7
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-14 14:39:10 +0800

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<main align="center">
+<h1>Welcome User To My Website</h1>
+<a href="gallery.php">Click here</a>
+</main>
+</body>
+</html>
