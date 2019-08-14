4b0d1bb33251b293a74ff55c07d37579bb6ebec9
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-14 09:51:51 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,14 +4,14 @@
 <title>Login</title>
 </head>
 <body>
- <h3 align="center">Gornez, John Enrick Digi Art Gallery</font></h3>
+ <h3 align="center">Gornez, John Enrick Digi Art Gallery</h3>
  <form action="gallery.php" align="center">
  <p><b>Create Username</b></p>
  <input type="text" name="" placeholder="Username" required>
  <p><b>Create Password</b></p>
- <input type="text" name="" placeholder='Password" required>
+ <input type="text" name="" placeholder="Password" required>
  <p><b>Confirm Password</b></p>
- <input type="text" name="" placeholder='Password" required>
+ <input type="text" name="" placeholder="Password" required>
  <button type="submit">Create account</button>
 </body>
 </html>
