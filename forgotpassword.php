d0b6b72b205920c6baade34b19a8aa5d282ca8e8
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-14 14:46:06 +0800

    Update forgotpassword.php

--- forgotpassword.php
+++ forgotpassword.php
@@ -5,7 +5,7 @@
 </head>
 <body>
  <h3 align="center">Gornez, John Enrick Digi Art Gallery</h3>
- <form action="index.php" align="center">
+ <form action="welcome.php" align="center">
  <label for="uname"><b>New password</b></label>
  <input type="text" name="password" placeholder="Password" required>
  <label for="uname"><b>Confirm password</b></label>
