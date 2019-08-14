03bbc7f0daca4486456f99e1cee9d7e88710a439
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-14 09:45:07 +0800

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+ <h3 align="center">Gornez, John Enrick Digi Art Gallery</font></h3>
+ <form action="gallery.php" align="center">
+ <p><b>Create Username</b></p>
+ <input type="text" name="" placeholder="Username" required>
+ <p><b>Create Password</b></p>
+ <input type="text" name="" placeholder='Password" required>
+ <p><b>Confirm Password</b></p>
+ <input type="text" name="" placeholder='Password" required>
+ <button type="submit">Create account</button>
+</body>
+</html>
