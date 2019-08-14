ae5d6d50c97c4e14021941b57eef3fb80babaf90
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-14 10:03:45 +0800

    Create forgot password.php

---
+++ forgot password.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+ <h3 align="center">Gornez, John Enrick Digi Art Gallery</h3>
+ <form action="index.php" align="center">
+ <label for="uname"><b>New password</b></label>
+ <input type="text" name="password" placeholder="Password" required>
+ <label for="uname"><b>Confirm password</b></label>
+ <input type="text" name="password" placeholder="Password" required>
+ <button type="submit">Reset</button
+ </body>
+ </html>
