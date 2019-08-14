93273766623b1aaafc4fadc3965bd24e86ca79ad
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-08-14 09:50:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 <title>Login</title>
 </head>
 <body>
- <h3 align="center">Gornez, John Enrick Digi Art Gallery</font></h3>
+ <h3 align="center">Gornez, John Enrick Digi Art Gallery</h3>
  <main align="center">
  <h2>Login Here</h2>
  <form action="gallery.php" align="center">
  <p><b>Username</b></p>
  <input type="text" name="" placeholder="Username" required>
  <p><b>Password</b></p>
- <input type="text" name="" placeholder='Password" required>
+ <input type="text" name="" placeholder="Password" required>
  <button type="submit">Login</button>
  </form>
  <a href="signup.php">Don't have an account yet?</a>
