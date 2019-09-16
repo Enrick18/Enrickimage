b3ed0fef36a88f6ea8931f89d0592d42900a6667
Enrick18 <53499837+Enrick18@users.noreply.github.com>
2019-09-16 10:04:24 +0800

    Update forgotpassword.php

--- forgotpassword.php
+++ forgotpassword.php
@@ -25,6 +25,8 @@
  <label for="uname"><b>New password</b></label>
  <input type="text" name="password" placeholder="Password" required>
   <br>
+  <br>
+  <br>
  <label for="uname"><b>Confirm password</b></label>
  <input type="text" name="password" placeholder="Password" required>
   <button type="submit">Reset</button>
